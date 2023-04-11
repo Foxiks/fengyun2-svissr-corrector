41cd52f8a2c7de0911abbcbb77b4f70faae6265c
Фукиш <egor158741@yandex.ru>
2023-04-11 13:02:10 +0300

    Update FengYun2-Counter-Corrector.py

--- src/FengYun2-Counter-Corrector.py
+++ src/FengYun2-Counter-Corrector.py
@@ -17,16 +17,16 @@ def create_counter():
             return i
     
     return func
-counter2 = create_counter()
-size = os.path.getsize(inputfile)
-total = int(size/44356)
-n = 1
-op = total/100
-last = total - int(op*11)
-first = int(op*3)
-t = first + 2501
-line = 88712
-backline = 0
+
+counter=create_counter()
+size=os.path.getsize(inputfile)
+total=int(size/44356)
+n=1
+op=total/100
+last=total-int(op*11)
+first=int(op*3)
+t=first+2501
+i=0
 syncword = "00000000000000"
 if __name__ == "__main__":
     print("-----------------------------------------------------------------")
@@ -38,40 +38,31 @@ if __name__ == "__main__":
     with open(inputfile, 'rb') as f1:
         hexdata1 = f1.read().hex()
     while True:
-        x = hexdata1[backline:line]
-        backline = line
-        line += 88712
+        x = hexdata1[i:i+88712]
+        i += 88712
         syncwordfile = x[:14]
-        if (n < first):
-            vcducounter = "0000"
-            syncword = "00000000000000"
-        if (n >= first <= t):
-            count1 = counter2()
-            hexdata = hex(count1).split('x')[-1]
-            if len(hexdata) == 1:
-                vcducounter = "000" + hexdata.upper()
-            elif len(hexdata) == 2:
-                vcducounter = "00" + hexdata.upper()
-            elif len(hexdata) == 3:
-                vcducounter = "0" + hexdata.upper()    
-            else:
-                vcducounter = hexdata.upper()
+        if (n<first):
+            vcducounter="0000"
+            syncword="00000000000000"
+        if (n>=first<=t):
+            count1=counter()
+            vcducounter=hex(count1).split('x')[-1].upper().zfill(4)
             syncword = "00000033FFFF00"  
-        if (n > t):
-            vcducounter = "0000"
-            syncword = "000000CC000000"
-        if (n >= last):
-            vcducounter = "0000"
-            syncword = "000000CC000000"
-        block1 = x[14:134]
-        block2 = x[138:88712]
+        if (n>t):
+            vcducounter ="0000"
+            syncword="000000CC000000"
+        if (n>=last):
+            vcducounter="0000"
+            syncword="000000CC000000"
+        block1=x[14:134]
+        block2=x[138:88712]
         with open(outfile, 'ab') as ff:
-            ff.write(binascii.unhexlify(syncword + block1 + vcducounter + block2))
-        proz = n/op
+            ff.write(binascii.unhexlify(syncword+block1+vcducounter+block2))
+        proz=n/op
         print("Total lines: " + str(total) + " | Line: " + str(n) + " | Frame Sync: 0x" + syncwordfile.upper() + " | New Frame Sync: 0x" + syncword.upper() + " | Progress: " + str("%.0f" % proz) + "%", end='\r')
-        n += 1
+        n+=1
         sizeout = os.path.getsize(outfile)
-        if(sizeout >= size):
+        if(sizeout>=size):
             print("")
             print("Done!", end='\r')
             break
